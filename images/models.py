ab7d9c8e15f18eed860358134a60d43c37bde351
Andres David Mendoza Marquez <andres.david.mm@hotmail.com>
2021-12-29 12:56:13 -0500

    changed img url char limit

--- images/models.py
+++ images/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from products.models import Product
 
 class Image(models.Model):
-    url = models.CharField(max_length=100)
+    url = models.URLField(max_length=300)
     product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True, related_name='images')
     
     def __str__(self):
