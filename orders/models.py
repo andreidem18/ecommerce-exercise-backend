71a4d243049ebed7b0fe559d5d2b8d63c2ff8482
Andres David Mendoza Marquez <andres.david.mm@hotmail.com>
2022-01-02 10:49:28 -0500

    added order name to django admin

--- orders/models.py
+++ orders/models.py
@@ -7,3 +7,8 @@ class Order(models.Model):
     product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True, related_name='orders')
     quantity = models.IntegerField()
     purchase_date = models.DateTimeField(auto_now_add=True)
+
+    
+    def __str__(self):
+        return self.product
+    
